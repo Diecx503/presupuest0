d4d1a64268719fd6c64d61afc5f220c61d772314
Didi <92070658+DidiRamos@users.noreply.github.com>
2021-10-10 21:40:13 -0600

    Update presupuesto.js

--- presupuesto/Recursos/Js/presupuesto.js
+++ presupuesto/Recursos/Js/presupuesto.js
@@ -1,7 +1,14 @@
-funtion tiempo(){
-var fecha= new Date();
-    document.write(fecha);
-    console.log(fecha).getMonth());
-    console.log(fecha).getFullYear());
+//función para fecha del presupuesto
+(function () {
+    var fecha = new Date();
+    var mes = fecha.getMonth();
+    var year = fecha.getFullYear();
 
-}
+var pMes = document.getElementById('mes');
+        pYear = document.getElementById('year');
+
+    var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    pMes.textContent = meses[mes];
+
+    pYear.textContent = year;
+}())
